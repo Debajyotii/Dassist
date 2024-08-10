490b65424f91ff8ca71bd86aef2159a24706e4dd
Debajyoti Dev <dv.aarghya@gmail.com>
2024-08-10 11:18:40 +0530

    SpringBoot Intial App

---
+++ dassist/src/main/java/com/example/dassist/DassistApplication.java
@@ -0,0 +1,13 @@
+package com.example.dassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DassistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DassistApplication.class, args);
+		System.out.println("Hello World!");
+	}
+}
